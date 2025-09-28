aa8ab33ed633bdb3b542c3810d9d2b59efd423e8
Miraç Kaan Şengül <mirac.kaansengull@gmail.com>
2025-09-28 15:59:37 +0300

    githubio değişiklik

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 import Services from './Pages/Services.jsx';
